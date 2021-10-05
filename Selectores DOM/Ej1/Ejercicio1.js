0bf3f5d32bb8abac5afd77c41736daeeb7dc361c
Javi Lira <74186769+macabros554@users.noreply.github.com>
2021-10-05 13:25:03 +0200

    Delete Selectores DOM directory

--- Selectores DOM/Ej1/Ejercicio1.js
+++
@@ -1,9 +0,0 @@
-let enlaces = document.querySelectorAll('a');
-let parrafos = document.querySelectorAll('p');
-let contEnlaces = document.querySelectorAll('a[href="http://prueba/"]');
-let respuesta = 'El numero de enlaces de la pagina son= ' + enlaces.length + "<br> <br>";
-respuesta = respuesta + 'El penultimo enlace apunta a: ' + document.querySelector('a:nth-last-child(2)') + "<br><br>";
-respuesta = respuesta + 'El enlace algo prueba se repite: ' + contEnlaces.length + " veces <br><br>";
-respuesta = respuesta + 'El tercer parrafo tiene= ' + document.querySelectorAll('p:nth-child(3) a').length + " enlaces <br>";
-
-document.write(respuesta);
--- Selectores DOM/Ej2/Ejercicio2op2.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('#enlace').addEventListener('click',cambiarClase)
-
-function cambiarClase() {
-    elExtra.classList.remove('oculto');
-    boton.classList.add('oculto');
-}
